7071bb89b0e66010f543d691b8e35464146c0467
JeHa-An <106300022+JeHa-An@users.noreply.github.com>
2023-01-30 18:01:21 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = input().rstrip()
+c = 1
+
+if int(N) < 10:
+    N = '0'+N[0]
+
+first = N
+
+while 1:
+    num_sum = int(N[0]) + int(N[1])
+    nf = N[-1] + str(num_sum)[-1]
+    N = nf
+
+    if N[0] == first[0] and N[1] == first[1]:
+        break
+    else:
+        c += 1
+print(c)
